7cad4f490a804f4a9bba34cfddcebb149b12d7d0
Aditya Kumar <aditya.kr.singh21@gmail.com>
2016-04-14 15:55:03 +0530

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 //Lets define a port we want to listen to
-const PORT=80; 
+const PORT=8000; 
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
